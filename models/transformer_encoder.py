e424e7b3a656d2bf86a0890ab6fe0381300076e5
Hyeonwoo9501 <dnfjrwkd9999@gmail.com>
2024-04-13 01:01:26 +0900

    [Refactor] transformer_encoder 모델에 linear layer 추가

--- models/transformer_encoder.py
+++ models/transformer_encoder.py
@@ -2,27 +2,12 @@ import torch
 import math
 import torch.nn as nn
 
-class TransformerEncoder(nn.Module):
-    def __init__(self, input_dim, d_model, num_heads, num_layers, dropout_rate=0.1):
-        super(TransformerEncoder, self).__init__()
-        self.input_linear = nn.Linear(input_dim, d_model)
-        self.pos_encoder = PositionalEncoding(d_model, dropout_rate)
-        encoder_layers = nn.TransformerEncoderLayer(d_model=d_model, nhead=num_heads, dropout=dropout_rate)
-        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers=num_layers)
-        self.d_model = d_model
-
-    def forward(self, src):
-        src = self.input_linear(src) * math.sqrt(self.d_model)
-        src = self.pos_encoder(src)
-        output = self.transformer_encoder(src)
-        return output
-
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super()(PositionalEncoding, self).__init__()
+        super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
 
-        position = torch.arange(max_len).unqueeze(1)
+        position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
         pe = torch.zeros(max_len, 1, d_model)
         pe[:, 0, 0::2] = torch.sin(position * div_term)
@@ -32,3 +17,20 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + self.pe[:x.size(0)]
         return self.dropout(x)
+
+class TransformerWithOutputLayer(nn.Module):
+    def __init__(self, input_dim, d_model, num_heads, num_layers, output_dim, dropout_rate=0.1):
+        super(TransformerWithOutputLayer, self).__init__()
+        self.input_linear = nn.Linear(input_dim, d_model)
+        self.pos_encoder = PositionalEncoding(d_model, dropout_rate)
+        encoder_layers = nn.TransformerEncoderLayer(d_model=d_model, nhead=num_heads, dropout=dropout_rate)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers=num_layers)
+        self.output_linear = nn.Linear(d_model, output_dim)
+        self.d_model = d_model
+
+    def forward(self, src):
+        src = self.input_linear(src) * math.sqrt(self.d_model)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src)
+        output = self.output_linear(output)
+        return output
