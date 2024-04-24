be8ef52982f6fa87f45435ee2235984f00daf6fe
Hyeonwoo9501 <dnfjrwkd9999@gmail.com>
2024-04-25 00:54:27 +0900

    [Feat] Add KMeansClustering Algorithm

---
+++ src/data_clustering.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from tslearn.clustering import TimeSeriesKMeans, KernelKMeans, KShape
+from tslearn.preprocessing import TimeSeriesScalerMeanVariance
+
+def KMeansClustering(data, n_clusters):
+    # DataFrame에서 NumPy 배열로 변환
+    data_np = data.values
+
+    # 데이터 스케일링
+    scaler = TimeSeriesScalerMeanVariance(mu=0., std=1.) # 평균 0, 표준편차 1로 정규화
+    data_scaled = scaler.fit_transform(data_np.reshape(data_np.shape[0], 1, data_np.shape[1]))
+
+    # 클러스터링 모델 생성 및 학습
+    model = TimeSeriesKMeans(n_clusters=n_clusters, metric="euclidean", max_iter=10)
+    labels = model.fit_predict(data_scaled)
+
+    result = data.iloc[:,:]
+    result['clusters'] = labels
+
+    return result
+
