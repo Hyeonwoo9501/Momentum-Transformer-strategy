828e9c1715b8a7ccea3d588f5b7d33148324d1d3
Hyeonwoo9501 <dnfjrwkd9999@gmail.com>
2024-04-26 00:35:10 +0900

    [Feat] Add KShapeClustering Algorithm code

--- src/data_clustering.py
+++ src/data_clustering.py
@@ -12,7 +12,7 @@ def KMeansClustering(data, n_clusters):
     data_scaled = scaler.fit_transform(data_np)
 
     # 클러스터링 모델 생성 및 학습
-    model = TimeSeriesKMeans(n_clusters=n_clusters, metric="euclidean", max_iter=10)
+    model = TimeSeriesKMeans(n_clusters=n_clusters, metric="euclidean", max_iter=50, n_init=5, random_state=0)
     labels = model.fit_predict(data_scaled)
 
     data['clusters'] = labels
@@ -28,10 +28,25 @@ def KernelKMeansClustering(data, kernel, n_clusters):
     data_scaled = scaler.fit_transform(data_np)
 
     # 클러스터링 모델 생성 및 학습
-    model = KernelKMeans(n_clusters=n_clusters, kernel=kernel, n_init=20, verbose=True, random_state=0)
+    model = KernelKMeans(n_clusters=n_clusters, kernel=kernel, max_iter=50, n_init=5, verbose=True, random_state=0)
     labels = model.fit_predict(data_scaled)
 
     data['clusters'] = labels
 
     return data
 
+def KShapeClustering(data, n_clusters):
+    # DataFrame에서 NumPy 배열로 변환
+    data_np = data.values
+
+    # 데이터 스케일링
+    scaler = TimeSeriesScalerMeanVariance(mu=0., std=1.) # 평균 0, 표준편차 1로 정규화
+    data_scaled = scaler.fit_transform(data_np)
+
+    # 클러스터링 모델 생성 및 학습
+    model = KShape(n_clusters=n_clusters, max_iter=50, n_init=5, verbose=True, random_state=0)
+    labels = model.fit_predict(data_scaled)
+
+    data['clusters'] = labels
+
+    return data
